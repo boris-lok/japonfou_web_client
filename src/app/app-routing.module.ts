fe6e0cb6bbc615b770fea8415e86103d530f0113
boris <boris.lok@cienet.com>
2022-06-23 15:51:59 +0800

    [Feature] Add customer listview component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {EditorComponent} from "./components/customer/editor/editor.component";
+import {ListviewComponent} from "./components/customer/listview/listview.component";
 
 const routes: Routes = [
   {
     path: 'customer/editor',
     component: EditorComponent
+  },
+  {
+    path: 'customers',
+    component: ListviewComponent,
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,13 @@ import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
 import {HttpClientModule} from "@angular/common/http";
 import {MatProgressBarModule} from "@angular/material/progress-bar";
+import { ListviewComponent } from './components/customer/listview/listview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EditorComponent
+    EditorComponent,
+    ListviewComponent
   ],
     imports: [
         BrowserModule,
